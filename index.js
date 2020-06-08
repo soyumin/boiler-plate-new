7ace92bd533d26387b76182dc1a2df4498917ba3
소유민 <soyumin@soyumin-ui-MacBookPro.local>
2020-06-08 19:49:35 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
 
-const mongoose = require("mongoose"); //몽구스를 이용해 몽고db와 앱을 연결
-mongoose.connect(
-  "mongodb+srv://soyumin:asdf1234@boiler-plate-new-l8fp5.mongodb.net/test?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  } //에러 방지하기위한..
-).then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+//bodyParser가 분석해서 server에 가져옴 urlencoded , json 으로 되어있는 파일을 분석함
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const mongoose = require("mongoose") //몽구스를 이용해 몽고db와 앱을 연결
+mongoose
+  .connect(
+    config.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    } 
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+  //에러 방지하기위한..
+
+app.get("/", (req, res) => res.send("Hello World! 뿡"));
+
+app.post("/register", (req, res) => {
+  // 회원가입할떄 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터베이스에 넣어준다
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    // 정보들이 user모델에 저장됨
+    if (err) return res.json({ success: false, err }); //저장할때 에러가 있으면
+    return res.status(200).json({
+      success: true
+    }); //회원가입에 성공했을경우
+  });
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
